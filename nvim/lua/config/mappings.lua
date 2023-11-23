8527e1a28ee544b27c88f86018743babf05b7912
younisa <younisa@student.chalmers.se>
2023-11-23 18:39:27 +0100

    [NVIM] Fix tab mappings bug

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -10,6 +10,6 @@ local keymap = vim.keymap
 keymap.set({ "n", "v" }, "<leader>y", '"+y', { noremap = true, silent = true })
 
 -- Map tab functionality
-keymap.set({"n"}, "<Tab>", ":+tabnext<CR>", { noremap = true, silent = true}) -- Move to next tab
-keymap.set({"n"}, "<Shift-Tab>", ":-tabnext<CR>", { noremap = true, silent = true}) -- Move to previous tab
+keymap.set({"n"}, "<Tab>", ":tabnext<CR>", { noremap = true, silent = true}) -- Move to next tab
+keymap.set({"n"}, "<S-Tab>", ":tabprev<CR>", { noremap = true, silent = true}) -- Move to previous tab
 
