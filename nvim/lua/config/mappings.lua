73cd64e86bc2bda71fc8b0576d618cb39c9a6394
younisa <younisa@student.chalmers.se>
2023-11-23 18:34:40 +0100

    [NVIM] Add mappings for tab navigation

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -4,6 +4,12 @@ vim.g.mapleader = " "
 -- refer to: https://m4xshen.dev/posts/build-your-modern-neovim-config-in-lua/
 -- vim.keymap.set({ mode }, { lhs }, { rhs }, { opts })
 
+local keymap = vim.keymap
+
 -- map leader+y to copy to system clipboard in normal and visual mode
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { noremap = true, silent = true })
+keymap.set({ "n", "v" }, "<leader>y", '"+y', { noremap = true, silent = true })
+
+-- Map tab functionality
+keymap.set({"n"}, "<Tab>", ":+tabnext<CR>", { noremap = true, silent = true}) -- Move to next tab
+keymap.set({"n"}, "<Shift-Tab>", ":-tabnext<CR>", { noremap = true, silent = true}) -- Move to previous tab
 
