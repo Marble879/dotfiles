9f6598285a041a7d5d69cb8e6cecd083ee2e2554
younisa <younisa@student.chalmers.se>
2023-12-08 21:28:19 +0100

    Change java lsp server

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,8 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },                 -- Load plugin whenever open new buffer/pre eixsitng file. Second one means load plugin when open new buffer/new file that doesnt exist
+  event = { "BufReadPre", "BufNewFile" },                     -- Load plugin whenever open new buffer/pre eixsitng file. Second one means load plugin when open new buffer/new file that doesnt exist
   dependencies = {
-    "hrsh7th/cmp-nvim-lsp",                               -- Completion source so have LSP reccomendation in auto completion
+    "hrsh7th/cmp-nvim-lsp",                                   -- Completion source so have LSP reccomendation in auto completion
     { "antosha417/nvim-lsp-file-operations", config = true }, -- lets us rename files via nvim tree + update any effected import statements
   },
   config = function()
@@ -15,7 +15,7 @@ return {
     local keymap = vim.keymap
 
     local opts = { noremap = true, silent = true } -- table of options for keymaps
-    local on_attach = function(client, bufnr)     -- allows us to define a set of keymaps that will only apply when a given language server attaches to a buffer
+    local on_attach = function(client, bufnr)      -- allows us to define a set of keymaps that will only apply when a given language server attaches to a buffer
       opts.buffer = bufnr
 
       -- set keybinds
@@ -29,7 +29,7 @@ return {
 
     -- Change the Diagnostic symbols in the sign column (gutter)
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do                                 -- go through signs table
+    for type, icon in pairs(signs) do                                  -- go through signs table
       local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" }) -- sets the icon for the different types
     end
@@ -100,6 +100,13 @@ return {
           },
         },
       },
+    })
+
+    -- Java
+    lspconfig["jdtls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+
     })
   end,
 }
